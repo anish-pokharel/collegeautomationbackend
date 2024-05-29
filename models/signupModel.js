42d0f00bdb57795def3d8328ca91d61a586d9238
Menuka Lamsal <lamshalashmita2020@gmail.com>
2024-05-29 14:56:08 -0500

    conflict on signupModel.js resolved

--- models/signupModel.js
+++ models/signupModel.js
@@ -1,15 +1,9 @@
 const mongoose= require('mongoose');
 
 const RegisterSchema= new mongoose.Schema({
-<<<<<<< HEAD
-    name:{type:String},
-    email:{type:String, unique:true, required:true},
-    // rollno:{type:Number, unique:true},
-=======
     name:{type:String}, 
     email:{type:String, unique:true},
     rollno:{type:Number, unique:true},
->>>>>>> 78771b6154824ecb19e5d6480153a62ed9cd9cac
     address:{type:String},
     password:{type:String},
     confirmPassword:{type:String},

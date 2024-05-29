8e209d18fa2891045b6635749d80717587aa8132
Anish Pokharel <anish.pokharel.710.ap@gmail.com>
2024-05-29 21:22:53 +0545

    index implementaion

--- index.js
+++ index.js
@@ -3,12 +3,18 @@ const app = express();
 const connectdb = require('./db');
 const cors = require('cors');
 const userRoutes= require('./routes/userRoute')
+const discussion= require('./routes/discussionRoutes')
+const joinClub= require('./routes/joinClubRoutes')
+const sponsorShip= require('./routes/sponsorshipRoutes')
 
 
 
 app.use(express.json());
 app.use(cors());
 app.use(userRoutes);
+app.use(discussion);
+app.use(joinClub);
+app.use(sponsorShip);
 app.listen(3200,()=>{
     console.log('LocalHost is connected');
 })

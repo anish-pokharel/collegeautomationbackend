61bffe91ef13fe625ffcfd4b59ac6d788ec26d77
Anish Pokharel <anish.pokharel.710.ap@gmail.com>
2024-05-30 09:07:44 +0545

    join club

--- models/joinClubModel.js
+++ models/joinClubModel.js
@@ -1,18 +1,12 @@
 const mongoose = require('mongoose');
 
 const addClubSchema = new mongoose.Schema({
-    name:{type:String },
-    program:{type: String},
-    semester:{type: String},
+   
     clubStatus:{
-        type:String,
-        enum:['political','non-political'],
-        default:'non-political'
-    },
+        type:String,require },
     clubName:{type:String},
-    position:{type:String},
-    reason:{ type: String, require},
-    joinedDate:{type: String}
+    
+    createdDate:{type: String}
 })
 const addClubname= mongoose.model('addClub',addClubSchema)
 module.exports= addClubname;

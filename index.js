3865b0b47cc9f5f88acd5d61aab50884e08fad8f
Anish Pokharel <anish.pokharel.710.ap@gmail.com>
2024-06-06 17:25:48 +0545

    model

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ const enrollment= require('./routes/enrollmentRoutes')
 const feedback= require('./routes/feedBackRoute')
 const anserAssignment= require('./routes/anserAssignmentRoute')
 const sponsorshipRequest= require('./routes/sponsorshipRequestRoute')
+const createDepartmentRoute= require('./routes/createDepartmentRoute')
 
 app.use(express.json());
 app.use(cors());
@@ -20,10 +21,11 @@ app.use(discussion);
 app.use(joinClub);
 app.use(addClub);
 app.use(enrollment);
-app.use(sponsorshipRequest);
+app.use(createDepartmentRoute);
 app.use(anserAssignment);
 app.use(feedback);
 app.use(sponsorShip);
+app.use(sponsorshipRequest);
 app.use(addUser);
 app.listen(3200,()=>{
     console.log('LocalHost is connected');
---
+++ routes/createDepartmentRoute.js
@@ -0,0 +1,69 @@
+
+const express = require('express');
+const router = express.Router();
+const createDepartmentListModel = require('../models/createDepartmentModel');
+const verifyToken = require('../middleware');
+
+
+
+
+
+
+
+router.post('/postDepartments', async (req, res) => {
+    try {
+        const department = new createDepartmentListModel(req.body);
+        await department.save();
+        res.status(201).send(department);
+    } catch (error) {
+        res.status(400).send(error);
+    }
+});
+
+router.get('/getDepartments', async (req, res) => {
+    try {
+        const departments = await createDepartmentListModel.find();
+        res.status(200).send(departments);
+    } catch (error) {
+        res.status(500).send(error);
+    }
+});
+
+router.get('/departments/:id', async (req, res) => {
+    try {
+        const department = await createDepartmentListModel.findById(req.params.id);
+        if (!department) {
+            return res.status(404).send('Department not found');
+        }
+        res.status(200).send(department);
+    } catch (error) {
+        res.status(500).send(error);
+    }
+});
+
+router.put('/departments/:id', async (req, res) => {
+    try {
+        const department = await createDepartmentListModel.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true });
+        if (!department) {
+            return res.status(404).send('Department not found');
+        }
+        res.status(200).send(department);
+    } catch (error) {
+        res.status(400).send(error);
+    }
+});
+
+router.delete('/departments/:id', async (req, res) => {
+    try {
+        const department = await createDepartmentListModel.findByIdAndDelete(req.params.id);
+        if (!department) {
+            return res.status(404).send('Department not found');
+        }
+        res.status(200).send(department);
+    } catch (error) {
+        res.status(500).send(error);
+    }
+});
+
+
+module.exports = router;
